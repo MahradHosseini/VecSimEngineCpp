fe58bfcae543ba99f342376eb4bc832300295f35
Mahrad Hosseini <mahrad.hosseini50@gmail.com>
2025-06-26 19:12:14 +0300

    Final Test Done

--- Tests.cpp
+++ Tests.cpp
@@ -77,7 +77,7 @@ int TestBgeEmbedderONNXRuntime(
     const std::string &onnxFile,
     const std::string &tokenizerFile,
     const std::size_t N
-    ) {
+) {
     using clock = std::chrono::high_resolution_clock;
     std::chrono::time_point<std::chrono::steady_clock> t0 = clock::now();
 
@@ -137,8 +137,8 @@ static Chat parseChatLine(const std::string &line) {
     static const std::regex messageRe(R"MSG(\{"role"\s*:\s*"([^"]+)"\s*,\s*"text"\s*:\s*"([^"]*)"\})MSG");
 
     for (std::sregex_iterator it = std::sregex_iterator(line.begin(), line.end(), messageRe);
-        it != std::sregex_iterator(); ++it) {
-            chat.messages.push_back({(*it)[1].str(), (*it)[2].str()});
+         it != std::sregex_iterator(); ++it) {
+        chat.messages.push_back({(*it)[1].str(), (*it)[2].str()});
     }
 
     static const std::regex skillsBlockRe(R"SKILLS("skills"\s*:\s*\[([^\]]*)\])SKILLS");
@@ -148,7 +148,7 @@ static Chat parseChatLine(const std::string &line) {
         std::string inner = skillsMatch[1].str();
         static const std::regex skillRe(R"SKILL("([^\"]+)")SKILL");
         for (std::sregex_iterator it = std::sregex_iterator(inner.begin(), inner.end(), skillRe);
-            it != std::sregex_iterator(); ++it) {
+             it != std::sregex_iterator(); ++it) {
             chat.skills.push_back((*it)[1].str());
         }
     }
@@ -161,7 +161,7 @@ void printChats(const std::vector<Chat> &chats) {
         const Chat &c = chats[i];
         std::cout << "Chat #" << (i + 1) << std::endl;
         std::cout << " Messages: " << c.messages.size() << std::endl;
-        for (const Message &m : c.messages) {
+        for (const Message &m: c.messages) {
             std::cout << "   [" << m.role << "] " << m.text << std::endl;
         }
         std::cout << "  Skills: ";
@@ -176,7 +176,7 @@ int TestVectorSimilarityEngine(
     const std::string &tokenizerFile,
     const std::string &embedderFile,
     const std::string &chatsFile
-    ) {
+) {
     using clock = std::chrono::high_resolution_clock;
     std::chrono::time_point<std::chrono::steady_clock> t0 = clock::now();
 
@@ -208,21 +208,64 @@ int TestVectorSimilarityEngine(
         ++lineNo;
         try {
             chats.push_back(parseChatLine(line));
-        }catch (const std::exception &e) {
+        } catch (const std::exception &e) {
             std::cerr << "Error: " << e.what() << '\n';
             return 1;
         }
     }
 
-    printChats(chats);
+    // printChats(chats);
+
+    std::size_t skillSize = skillPool.size();
+    std::vector<int64_t> hits(skillSize, 0);
+    int64_t total{0};
 
     try {
         VectorSimilarityEngine engine(skillPool, tokenizerFile, embedderFile);
 
-    }catch (const std::exception &e) {
+        for (std::size_t i = 0; i < chats.size(); ++i) {
+            std::cout << "\r" << "Processing chat " << (i + 1) << "/" << chats.size() << std::flush;
+            Chat &c = chats[i];
+            total += c.skills.size();
+            std::string cStr;
+            for (Message &m: c.messages) {
+                cStr += "\n" + m.text;
+            }
+            // std::cout << "   Chat string: " << cStr << std::endl;
+            VectorSimilarityEngine::SkillAndScoreVector tops = engine.getTopSkills(cStr, skillSize);
+            for (std::size_t k = 0; k < tops.size(); ++k) {
+                for (std::string &s: c.skills) {
+                    if (tops[k].first == s) {
+                        // std::cout << "      Hit found at " << k << " for: " << tops[k].first << std::endl;
+                        hits[k] += 1;
+                    }
+                }
+            }
+        }
+    } catch (const std::exception &e) {
         std::cerr << "Error: " << e.what() << '\n';
         return 1;
     }
+
+    std::cout << "Total Skills: " << total << std::endl;
+    int64_t totalHits{0};
+    for (int64_t k: hits) {
+        totalHits += k;
+    }
+    std::cout << "Total hits: " << totalHits << std::endl;
+    for (std::size_t i = 0; i < skillSize; ++i) {
+        std::cout << "   Hit Rate @" << (i + 1) << ": " << static_cast<double>(hits[i]) * 100 / total << "%" <<
+                std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "Cumulative Accuracy: " << std::endl;
+    int64_t cumHits{0};
+    for (std::size_t i = 0; i < skillSize; ++i) {
+        cumHits += hits[i];
+        std::cout << "   @" << (i + 1) << ": " << static_cast<double>(cumHits) * 100 / total << "%" << std::endl;
+    }
+
 
     std::chrono::time_point<std::chrono::steady_clock> t1 = clock::now();
     long long ms = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
